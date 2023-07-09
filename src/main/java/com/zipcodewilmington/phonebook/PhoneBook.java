eb6a9c339cc1df3a9e6d304f0eded12818ca8cc4
Angela Kim <aangelakkim@gmail.com>
2023-07-09 16:38:26 -0400

    passed AddAll and Constructor tests

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -2,7 +2,7 @@ package com.zipcodewilmington.phonebook;
 
 import java.util.List;
 import java.util.ArrayList;
-//import java.util.HashMap;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -16,32 +16,62 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = map; //phonebook map is initialized with the provided argument map
     }
 
     public PhoneBook() {
-        this(null);
+        this.phonebook = new HashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        List<String> number = phonebook.getOrDefault(name, new ArrayList<>()); // gets value at key name, if it doesn't exist a new empty list is created
+        number.add(phoneNumber); // adding phone number to the arraylist
+        phonebook.put(name,number); //adding key/value name/list of phone numbers to the phonebook
     }
 
-    public void addAll(String name, String... phoneNumbers) {
+    public void addAll(String name, String... phoneNumbers) { // the ... is called varargs syntax. allows you to pass an arbitrary number of values of the specified type
+        List<String> number = phonebook.getOrDefault(name, new ArrayList<>());
+        for(String phoneNumber: phoneNumbers) { //iterate over arbitrary number of phone numbers and add to arraylist
+            number.add(phoneNumber);
+        }
+        phonebook.put(name,number); // add name/list of phone numbers to phonebook
     }
 
     public void remove(String name) {
+        phonebook.remove(name);
     }
+
 
     public Boolean hasEntry(String name) {
-        return null;
+        return phonebook.containsKey(name);
     }
 
+
+    public Boolean hasEntry(String name, String phoneNumbers) {
+        boolean entryExist = false;
+        for(Map.Entry<String, List<String>> entry: phonebook.entrySet()) {
+            if((entry.getValue().equals(phoneNumbers)) && (entry.getKey().equals(name))) {
+                entryExist = true;
+            }
+        }
+        return entryExist;
+    }
+
+
+
+
     public List<String> lookup(String name) {
-        return null;
+        return phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        String key = "";
+        for(Map.Entry<String, List<String>> entry : phonebook.entrySet()) { //Map.Entry is the way to iterate through and obtain collection view of the map(entrySet)
+            if(entry.getValue().equals(phoneNumber)) {
+                key = entry.getKey();
+            }
+        }
+        return key;
     }
 
     public List<String> getAllContactNames() {
@@ -49,6 +79,6 @@ public class PhoneBook {
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
