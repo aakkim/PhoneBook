982ca54f4158abdc23e755dca9dcf7f589f28699
Angela Kim <aangelakkim@gmail.com>
2023-07-12 18:48:53 -0400

    lab completed

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,12 +1,6 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
@@ -14,75 +8,69 @@ import java.util.Set;
  */
 public class PhoneBook {
 
-    private final Map<String, List<String>> phonebook;
+    private final Map<String, List<String>> phonebook; // key is a string, values is a list of strings
 
     public PhoneBook(Map<String, List<String>> map) {
         this.phonebook = map; //phonebook map is initialized with the provided argument map
     }
 
     public PhoneBook() {
-        this.phonebook = new LinkedHashMap<>();
-    }
+        this(new LinkedHashMap<>()); //nullary constructor set this object to a LinkedHashMap when no parameters are provided
+    } //LinkedHashMap orders in which elements were inserted
 
     public void add(String name, String phoneNumber) {
-        List<String> number = phonebook.getOrDefault(name, new ArrayList<>()); // gets value at key name, if it doesn't exist a new empty list is created
-        number.add(phoneNumber); // adding phone number to the arraylist
-        phonebook.put(name,number); //adding key/value name/list of phone numbers to the phonebook
+        if(phonebook.containsKey(name)) { //if name (key) exists in phonebook
+            phonebook.get(name).add(phoneNumber);// get the existing name and add phone number to end of list
+        } else {
+            phonebook.put(name, new ArrayList<>()); // if name is not in phonebook, create new phone number list
+            phonebook.get(name).add(phoneNumber); // get the name and add the phone number to the array list associated with that name
+            // get the name (key) and gives you the value (empty array list/nothing) and you add the phoneNumber to the list
+        }
+
     }
 
-    public void addAll(String name, String... phoneNumbers) { // the ... is called varargs syntax. allows you to pass an arbitrary number of values of the specified type
-        List<String> number = phonebook.getOrDefault(name, new ArrayList<>());
-        for(String phoneNumber: phoneNumbers) { //iterate over arbitrary number of phone numbers and add to arraylist
-            number.add(phoneNumber);
+    public void addAll(String name, String... phoneNumbers) {
+        for(String p : phoneNumbers) {
+            add(name,p); // iterating through phoneNumbers, and adding name and each phonenumbers.
         }
-        phonebook.put(name,number); // add name/list of phone numbers to phonebook
     }
 
     public void remove(String name) {
-        phonebook.remove(name);
+        phonebook.remove(name); //remove name(key) from phonebook(map)
     }
 
-
-    public Boolean hasEntry(String name) {
-        return phonebook.containsKey(name);
-    }
-
-
-    public Boolean hasEntry(String name, String phoneNumbers) {
-        boolean entryExist = false;
-        for(Map.Entry<String, List<String>> entry: phonebook.entrySet()) {
-            if((entry.getValue().equals(phoneNumbers)) && (entry.getKey().equals(name))) {
-                entryExist = true;
-            }
+    public Boolean hasEntry(String name, String phoneNumber) { // checking to see if name and phone number are in phonebook together
+        if(phonebook.containsKey(name) && phonebook.get(name).contains(phoneNumber)) {
+            return true; //if phonebook has name(key) and that name(key) has phonenumbers in its value list, return true
         }
-        return entryExist;
+        return false;
     }
 
-
+    public Boolean hasEntry(String name) { // check to see if name is in phonebook
+        return phonebook.containsKey(name); //.containsKey returns boolean so if name(key) is in map, it will return true
+    }
 
 
     public List<String> lookup(String name) {
-        return phonebook.get(name);
+        return phonebook.get(name); //to return the list of phone numbers, get the name from phone book and list will be returned
     }
 
     public String reverseLookup(String phoneNumber)  {
-        String key = "";
-        for(Map.Entry<String, List<String>> entry : phonebook.entrySet()) { //Map.Entry is the way to iterate through and obtain collection view of the map(entrySet)
-            if(entry.getValue().equals(phoneNumber)) {
-                key = entry.getKey();
+        String reverse = ""; //declare and initialize a new string to return the key that contains the phone number
+        for(String p: phonebook.keySet()) { //.keySet create a set of keys in a map
+            if(phonebook.get(p).contains(phoneNumber)) { //check if a key contains the phone number argument
+                reverse = p; //set the variable to that key
             }
         }
-        return key;
+        return reverse;// return key
     }
 
     public List<String> getAllContactNames() {
-        LinkedHashMap<String, List<String>> map = new LinkedHashMap<>(phonebook);
-        Set<String> names = map.keySet();
-        List<String> getAll = new ArrayList<>(names);
-        return getAll;
+        List<String> names = new ArrayList<>(phonebook.keySet()); //Converting a set of keys to a list
+        return names;
     }
 
     public Map<String, List<String>> getMap() {
-        return phonebook;
+        return phonebook; //get the map which is the phonebook
     }
 }
